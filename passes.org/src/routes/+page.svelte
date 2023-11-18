0fdaa1b333f16b42ae4fc0a3cf61e0c043ed6c92
danscan <dan@dscanlon.com>
2023-11-18 12:43:35 -0500

    tweak(passes.org): mobile spacing fixes

--- passes.org/src/routes/+page.svelte
+++ passes.org/src/routes/+page.svelte
@@ -1,24 +1,24 @@
-<div class="min-h-screen p-10 bg-white dark:bg-black md:p-16 font-text">
+<div class="min-h-screen p-10 bg-white dark:bg-black md:p-16 font-text dark:text-white">
 	<!-- Header -->
   <div class="flex items-center justify-between pb-5 border-b border-black lg:pb-10 dark:border-white">
     <!-- Logo -->
     <div class="flex items-center gap-5">
       <svg class="w-14 h-9" viewBox="0 0 140 90" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="140" height="90" rx="10" fill="black"/>
-        <rect x="80" y="50" width="10" height="10" fill="white"/>
-        <rect x="90" y="40" width="10" height="10" fill="white"/>
-        <rect x="80" y="40" width="10" height="10" fill="white"/>
-        <rect x="70" y="40" width="10" height="10" fill="white"/>
-        <rect x="60" y="30" width="10" height="10" fill="white"/>
-        <rect x="60" y="40" width="10" height="10" fill="white"/>
-        <rect x="60" y="50" width="10" height="10" fill="white"/>
-        <rect x="50" y="50" width="10" height="10" fill="white"/>
-        <rect x="50" y="40" width="10" height="10" fill="white"/>
-        <rect x="50" y="30" width="10" height="10" fill="white"/>
-        <rect x="50" y="30" width="10" height="10" fill="white"/>
-        <rect x="40" y="30" width="10" height="10" fill="white"/>
-        <rect x="40" y="40" width="10" height="10" fill="white"/>
-        <rect x="40" y="50" width="10" height="10" fill="white"/>
+        <rect width="140" height="90" rx="10" class="fill-black dark:fill-white"/>
+        <rect x="80" y="50" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="90" y="40" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="80" y="40" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="70" y="40" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="60" y="30" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="60" y="40" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="60" y="50" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="50" y="50" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="50" y="40" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="50" y="30" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="50" y="30" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="40" y="30" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="40" y="40" width="10" height="10" class="fill-white dark:fill-black"/>
+        <rect x="40" y="50" width="10" height="10" class="fill-white dark:fill-black"/>
       </svg>
       <span class="text-xl font-rounded">Passes</span>
     </div>
@@ -28,18 +28,18 @@
   </div>
 
   <!-- Hero Content -->
-  <main class="flex flex-col items-start w-4/5 px-12 py-24 space-y-10">
-    <h1 class="leading-tight font-display text-7xl">
-      Many Apps. One Account.<br/>No Passwords.
+  <main class="flex flex-col items-start py-24 space-y-10 lg:px-12 lg:w-4/5">
+    <h1 class="text-5xl leading-tight font-display lg:text-7xl whitespace-break-spaces">
+      Many Apps. One Account. No Passwords.
     </h1>
     <p class="text-2xl font-text">
       Passes put users in control of their identity and privacy by giving them a common client-side identity that any app can request to identify them or do something on their behalf.
     </p>
 
-    <a href="//docs.passes.org" class="flex items-baseline gap-4 px-6 py-5 text-lg text-white bg-black rounded">
+    <a href="//docs.passes.org" class="flex items-baseline gap-4 px-6 py-5 text-lg text-white bg-black rounded dark:bg-white dark:text-black">
       <span>Developer Docs</span>
-      <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M4 0H12V8H10V4H8V6H6V8H4V10H2V12H0V10H2V8H4V6H6V4H8V2H4V0Z" fill="white"/>
+      <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" class="fill-white dark:fill-black">
+        <path d="M4 0H12V8H10V4H8V6H6V8H4V10H2V12H0V10H2V8H4V6H6V4H8V2H4V0Z" />
       </svg>        
     </a>
   </main>
