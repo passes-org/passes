fd31a68b3d41d77efacda04b2d41c51b9546ff06
danscan <dan@dscanlon.com>
2023-11-18 12:32:03 -0500

    feat(passes.org): add new home page content

--- passes.org/src/routes/+page.svelte
+++ passes.org/src/routes/+page.svelte
@@ -1,11 +1,46 @@
-<div class="min-h-screen dark:bg-gray-800 bg-gray-100 p-5">
-	<main class="rounded-xl bg-white dark:bg-gray-950 p-4 space-y-4 font-sans container mx-auto">
-    <h1 class="text-2xl font-bold">Passes</h1>
-    <nav>
-      <a href="/">Try</a>
-      <a href="/">Docs</a>
-      <a href="/">Github</a>
-    </nav>
+<div class="min-h-screen p-10 bg-white dark:bg-black md:p-16 font-text">
+	<!-- Header -->
+  <div class="flex items-center justify-between pb-5 border-b border-black lg:pb-10 dark:border-white">
+    <!-- Logo -->
+    <div class="flex items-center gap-5">
+      <svg class="w-14 h-9" viewBox="0 0 140 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect width="140" height="90" rx="10" fill="black"/>
+        <rect x="80" y="50" width="10" height="10" fill="white"/>
+        <rect x="90" y="40" width="10" height="10" fill="white"/>
+        <rect x="80" y="40" width="10" height="10" fill="white"/>
+        <rect x="70" y="40" width="10" height="10" fill="white"/>
+        <rect x="60" y="30" width="10" height="10" fill="white"/>
+        <rect x="60" y="40" width="10" height="10" fill="white"/>
+        <rect x="60" y="50" width="10" height="10" fill="white"/>
+        <rect x="50" y="50" width="10" height="10" fill="white"/>
+        <rect x="50" y="40" width="10" height="10" fill="white"/>
+        <rect x="50" y="30" width="10" height="10" fill="white"/>
+        <rect x="50" y="30" width="10" height="10" fill="white"/>
+        <rect x="40" y="30" width="10" height="10" fill="white"/>
+        <rect x="40" y="40" width="10" height="10" fill="white"/>
+        <rect x="40" y="50" width="10" height="10" fill="white"/>
+      </svg>
+      <span class="text-xl font-rounded">Passes</span>
+    </div>
+
+    <!-- Icon: Github -->
+    <a href="//github.com/passes-org/passes">Github</a>
+  </div>
+
+  <!-- Hero Content -->
+  <main class="flex flex-col items-start w-4/5 px-12 py-24 space-y-10">
+    <h1 class="leading-tight font-display text-7xl">
+      Many Apps. One Account.<br/>No Passwords.
+    </h1>
+    <p class="text-2xl font-text">
+      Passes give users a common identity that can be requested on the client by any app. No information is shared without the user's explicit consent. Passes are a simple, secure, and private way to authenticate users.
+    </p>
+
+    <a href="//docs.passes.org" class="flex items-baseline gap-4 px-6 py-5 text-lg text-white bg-black rounded">
+      <span>Developer Docs</span>
+      <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M4 0H12V8H10V4H8V6H6V8H4V10H2V12H0V10H2V8H4V6H6V4H8V2H4V0Z" fill="white"/>
+      </svg>        
+    </a>
   </main>
-  <div class="text-gray-400 dark:text-gray-600 text-center text-sm py-4">Passes Protocol</div>
 </div>
