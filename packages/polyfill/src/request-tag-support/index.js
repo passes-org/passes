db128ac5d8cce42462cb3231254357ac10122446
danscan <dan@dscanlon.com>
2023-12-05 14:10:19 -0500

    feat(polyfill): remove request-tag-support (no longer needed)

--- packages/polyfill/src/request-tag-support/index.js
+++
@@ -1,6 +0,0 @@
-import * as passesDevV1SetPassEngine from './passesDevV1SetPassEngine';
-
-/** @type {Record<string, (request: Uint8Array) => Promise<Uint8Array>>} */
-export const SUPPORTED_REQUEST_TAGS = {
-  [passesDevV1SetPassEngine.REQUEST_TAG]: passesDevV1SetPassEngine.support,
-};
