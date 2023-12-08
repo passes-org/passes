27a3c509c43971707b0516fc21b42699e64e9cce
danscan <dan@dscanlon.com>
2023-12-08 03:32:19 -0500

    chore: upgrade polyfill

--- passes.org/src/routes/request/+page.server.js
+++ passes.org/src/routes/request/+page.server.js
@@ -1,35 +1,19 @@
 import { redirect } from "@sveltejs/kit";
-import { z } from "zod";
-import { zfd } from "zod-form-data";
-import { PassProviders, parseRequestTag } from "../../../../packages/reqs";
 import { getUserPassProvider } from "../../lib/userPassProvider.server";
-import { ServerTransportCodec } from "../../lib/SpringBoard/ServerTransportCodec";
 
-/** @type {import('./$types').Actions} */
-export const actions = {
-  default: async (event) => {
-    const { request } = await RequestBodySchema.parseAsync(await event.request.formData());
-    const referrerValue = event.request.headers.get('referer');
-    const referrer = referrerValue ? new URL(referrerValue).host : undefined;
-
-    const requestTag = parseRequestTag(request);
-    
-    // Redirect any pass request except setPassProvider to the user's pass provider.
-    const userPassProvider = getUserPassProvider(event);
-    if (userPassProvider && requestTag !== PassProviders.setPassProvider.requestTag) {
-      throw redirect(307, userPassProvider);
-    }
-
-    return {
-      referrer,
-      requestBase64Url: ServerTransportCodec.encode(request),
-      requestTag,
-    };
+/** @type {import('./$types').PageServerLoad} */
+export const load = async (event) => {
+  const referrerValue = event.request.headers.get('referer');
+  const referrer = referrerValue ? new URL(referrerValue).host : undefined;
+  
+  // Redirect any pass request except setPassProvider to the user's pass provider.
+  const userPassProvider = getUserPassProvider(event);
+  if (userPassProvider) {
+    throw redirect(307, userPassProvider);
   }
-};
 
-const RequestBodySchema = zfd.formData({
-  request: zfd.file()
-    .transform(async (file) => new Uint8Array(await file.arrayBuffer()))
-    .pipe(z.instanceof(Uint8Array)),
-});
+  return {
+    referrer,
+    userPassProvider,
+  };
+};
--- passes.org/src/routes/request/+page.svelte
+++ passes.org/src/routes/request/+page.svelte
@@ -1,17 +1,11 @@
 <script>
-	import { ServerTransportCodec } from "$lib/SpringBoard/ServerTransportCodec";
-	import SpringBoard from "$lib/SpringBoard/SpringBoard.svelte";
+  export let data;
 
-  export let form;
-  if (!form) throw new Error('No form data');
-
-  /** @type {Window | undefined} */
-  let opener = typeof window !== 'undefined'
-    ? window.opener ?? window.parent
-    : undefined;
+  // /** @type {Window | undefined} */
+  // let opener = typeof window !== 'undefined'
+  //   ? window.opener ?? window.parent
+  //   : undefined;
 </script>
 
-<SpringBoard
-  rawRequest={ServerTransportCodec.decode(form.requestBase64Url)}
-  referrer={form.referrer ?? opener?.location.host ?? 'unknown referrer'}
-/>
+<!-- TODO: FIXME: Add back SpringBoard dev tool -->
+<div>{data.userPassProvider}</div>
