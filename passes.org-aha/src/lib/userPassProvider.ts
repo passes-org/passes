97b9584cf5697e97fd602220066b4cc2b85ffc63
Forest Plasencia <forestplasencia@me.com>
2024-01-17 10:08:33 -0800

    tweak(Passes): CR - Add astro-htmx to astro config, Fix types in userPassProvider, htmx cleanup

--- passes.org-aha/src/lib/userPassProvider.ts
+++ passes.org-aha/src/lib/userPassProvider.ts
@@ -1,34 +1,29 @@
-import { type APIContext, type AstroGlobal } from "astro";
+import { type AstroCookies } from "astro";
 
 const PASS_PROVIDER_COOKIE_NAME = 'user_pass_provider';
 
 /**
  * Returns the URI of the user's pass provider, if it's set.
- * @param
- * @returns {string | undefined}
  */
 
-export function getUserPassProvider({ cookies }: AstroGlobal) {
+export function getUserPassProvider({ cookies }: { cookies: AstroCookies }): string | undefined {
   return cookies.get(PASS_PROVIDER_COOKIE_NAME)?.value
 }
 
 /**
  * Sets the URI of the user's pass provider.
- 
- * @param {string} uri
- * @returns {void}
  */
-export function setUserPassProvider({ cookies }: AstroGlobal, uri: string) {
+
+export function setUserPassProvider({ cookies }: { cookies: AstroCookies }, uri: string): void {
   const expires = new Date();
   expires.setFullYear(expires.getFullYear() + 10);
-
   cookies.set(PASS_PROVIDER_COOKIE_NAME, uri, { sameSite: 'lax', path: '/', expires });
 }
 
 /**
  * Deletes the user pass provider cookie.
- * @returns {void}
  */
-export function clearUserPassProvider(astro: APIContext) {
-  astro.cookies.delete(PASS_PROVIDER_COOKIE_NAME, { path: '/' })
+
+export function clearUserPassProvider({ cookies }: { cookies: AstroCookies }): void {
+  cookies.delete(PASS_PROVIDER_COOKIE_NAME, { path: '/' })
 }
