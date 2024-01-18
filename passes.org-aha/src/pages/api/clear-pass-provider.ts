d154b49e95fc73b3ecaf3ea259a435901f9212ea
danscan <dan@dscanlon.com>
2024-01-18 02:54:01 -0500

    cleanup: use spacing instead of tabs for consistency

--- passes.org-aha/src/pages/api/clear-pass-provider.ts
+++ passes.org-aha/src/pages/api/clear-pass-provider.ts
@@ -2,14 +2,14 @@ import type { APIRoute } from "astro";
 import { clearUserPassProvider } from "../../lib/userPassProvider";
 
 export const POST: APIRoute = (context) => {
-    const url = new URL(context.request.url);
-    const forbidden = context.request.headers.get('origin') !== url.origin;
-    
-    if (forbidden) return new Response('Cross-origin POST not allowed', { status: 403 });
+  const url = new URL(context.request.url);
+  const forbidden = context.request.headers.get('origin') !== url.origin;
   
-    clearUserPassProvider(context);
-  
-    return new Response('OK', { status: 200, headers: {
-        "HX-Refresh": "true"
-    } });
+  if (forbidden) return new Response('Cross-origin POST not allowed', { status: 403 });
+
+  clearUserPassProvider(context);
+
+  return new Response('OK', { status: 200, headers: {
+    "HX-Refresh": "true"
+  } });
 }
