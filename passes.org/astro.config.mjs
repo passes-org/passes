e5cdd1d937fa5528f31ba55584267f5d873b9639
danscan <dan@dscanlon.com>
2024-01-18 03:03:38 -0500

    feat(passes.org): add Vercel adapter

--- passes.org/astro.config.mjs
+++ passes.org/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercelServerless from '@astrojs/vercel/serverless';
 import htmx from "astro-htmx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [htmx(), tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: vercelServerless(),
 });
