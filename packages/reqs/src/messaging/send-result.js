d270bec30c58486b0d3b3b8d09bb0ebf03a25823
danscan <dan@dscanlon.com>
2024-01-19 01:53:44 -0500

    feat(reqs): change sendResult to accept an encoded result

--- packages/reqs/src/messaging/send-result.js
+++ packages/reqs/src/messaging/send-result.js
@@ -1,13 +1,10 @@
 /**
  * Topic and Pass Providers should call `sendResult` when they have a result to send back to the requesting app.
- * @template TRequestBody
- * @template TResultBody
- * @param {import("../request-topic").RequestTopic<TRequestBody, TResultBody>} requestTopic
- * @param {import("../request-topic").RequestResult<TResultBody>} result
+ * @param {Uint8Array} result - The encoded result to send back to the requesting app.
  * @param {string} [origin='*'] - The origin to send the result to. Defaults to '*'.
  * @memberof Messaging
  */
-export async function sendResult(requestTopic, result, origin = '*') {
+export function sendResult(result, origin = '*') {
   if (typeof window === 'undefined') throw new Error('sendResult is only available in the browser');
   const requester = window.opener ?? window.parent;
   if (!requester) throw new Error('sendResult must be called from a popup or iframe');
@@ -15,7 +12,7 @@ export async function sendResult(requestTopic, result, origin = '*') {
   /** @type {import("../browser-types.jsdoc.mjs").ResultMessage} */
   const resultMessage = {
     type: 'org.passes.messaging.result',
-    result: await requestTopic.encodeResult(result),
+    result: result,
   };
 
   requester.postMessage(resultMessage, origin);
